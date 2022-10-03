c2cb603ba946c97e65f8f78cc709d5b07d75141c
i476933@fhict.nl <476933@student.fontys.nl>
2022-10-03 10:25:57 +0200

    Initial commit.

---
+++ src/test/java/YenloBE/YenloBE/YenloBeApplicationTests.java
@@ -0,0 +1,13 @@
+package YenloBE.YenloBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YenloBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
