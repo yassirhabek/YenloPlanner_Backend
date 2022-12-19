e5aa70763f8632e6892dc9473da7880e2c3fe794
fontysMatthijs <484020@student.fontys.nl>
2022-12-19 10:43:26 +0100

    Changed JWT expiration to 30 minutes

--- src/main/java/YenloBE/YenloBE/Security/SecurityConstants.java
+++ src/main/java/YenloBE/YenloBE/Security/SecurityConstants.java
@@ -1,6 +1,6 @@
 package YenloBE.YenloBE.Security;
 
 public class SecurityConstants {
-    public static final long JWT_EXPIRATION = 70000;
+    public static final long JWT_EXPIRATION = 1800000;
     public static final String JWT_SECRET = "7158890107710441921695630644172561604793";
 }
